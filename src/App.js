60b105f1cc3f8944cc5d6726f081d2280cf29a35
isaac <idreyes@uh.edu>
2022-04-11 18:17:32 -0500

    working on front end

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import personsService from './services/persons.js'
 import PersonForm from './components/PersonForm.js'
 import Records from './components/Records.js'
 const App = () => {
-    const [persons, setPersons] = useState([
-            {name: "Isaac Reyes", number: "09-22722", id: 1},
-            {name: "Teresa Mur", number: "08-22022", id: 2},
-            {name: "Tristan Uno", number: "03-28222", id: 3},
-            {name: "Joseph Roj", number: "07-22622", id: 4},
-        ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newPhone, setNewPhone] = useState('')
+    useEffect(() => {
+            personsService
+            .getAll()
+            .then(response => {
+                setPersons(response)
+            })
+    }, [])
     const handleNewPerson = (event) => {
         setNewName(event.target.value) 
     }
@@ -20,16 +24,52 @@ const App = () => {
     const handleSubmit = (event) => {
         event.preventDefault()
         const checkName = persons.find(person => person.name === newName)
-        if(checkName)
-            alert(`${newName} is already in the phonebook`)
+        if(checkName){
+            const updateNum = window.confirm(`${newName} is already in the phonebook. would you like to replace the old number with the new one?`)
+            //update object's number
+            //use update function
+            //add returned object to list
+            if(!updateNum)
+                return
+            const updatedPerson = { ...checkName, number:newPhone  }
+            personsService
+                .update(checkName.id, updatedPerson)
+                .then(returnedPerson => {
+                    setPersons(persons.map(person => person.id !== checkName.id ? person : returnedPerson))
+                })
+        }
         else if(!checkName){
         const newPerson = {
             name: newName,
             number: newPhone,
             id: persons.length+ 1
         }
+        personsService
+            .create(newPerson)
+            .then(returnedPerson => {
+                setPersons(persons.concat(returnedPerson))
+                setNewName('')
+                setNewPhone('')
+                })
         setPersons(persons.concat(newPerson))
-            }}
+            }
+    }
+    const handleDel = id => {
+        const maybe = window.confirm("Do you really want to delete record?")
+        if(!maybe)
+            return
+        const toDel = persons.find(person => person.id === id)
+        //console.log(toDel.name)
+        personsService
+            .del(id)
+            .then(persontoDel => {
+                setPersons(persons.filter(person => person.id !== toDel.id ))
+            })
+            .catch(error => {
+                alert(`person '${toDel.name}' has already been deleted`)
+                setPersons(persons.filter(person => person.id !== toDel.id ))
+            })
+    }
    return(
         <div>
             <h2> Phonebook </h2>
@@ -41,7 +81,7 @@ const App = () => {
                 handleSubmit = {handleSubmit}
             />
             <h3> Records </h3>
-            <Records persons = {persons}/>
+            <Records persons = {persons} handleDel= {handleDel}/>
         </div>
     )
 }
--- src/components/Records.js
+++ src/components/Records.js
@@ -1,10 +1,15 @@
 import React from 'react'
-const Records = ({persons}) =>
+const Records = ({persons, handleDel}) =>
 {
     return(
         <div>
         {persons.map(person => 
-           <p key={person.id}> {person.name} {person.number}</p>)}
+           <p 
+                key={person.id}> 
+                {person.name} 
+                {person.number}
+                <button onClick={() => handleDel(person.id)}> Delete</button>
+            </p>)}
         </div>
         )
 }
